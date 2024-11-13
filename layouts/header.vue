3f1c982ad335d7f0cd6c5f8664e3b8683ed644fe
batinhvo <batinhvo@gmail.com>
2024-11-13 18:22:41 +0700

    13112024

--- layouts/header.vue
+++ layouts/header.vue
@@ -2,7 +2,7 @@
     <header class="bg-white shadow-md">
         <!-- top-header -->
         <div class="border-b border-gray-100 py-2"> 
-            <div class="container max-w-xl flex justify-between">
+            <div class="container mx-auto max-w-xl flex justify-between">
                 <div class="topbar-left">
                     <a class="text-gray-110 text-font-13" href="https://beta.vinashoptv.com/">Welcome to Vinashop</a>
                 </div>
@@ -14,39 +14,59 @@
             </div>
         </div>
         <!-- main-header -->
-        <div class="container mx-auto flex items-center px-6 py-2 xl:py-8">
-            <!-- Logo -->
-            <nav class="max-width-270 w-2/6">           
-                <a class="logo" href="/">
-                    <img src="/images/logo-01.png" alt="">
-                </a>
-            </nav>
-            <!-- search -->
-            <div class="w-3/6">
-                <form action="">
-                    <div class="input-group flex">
-                        <input type="search" class="border-2 border-primary rounded-bl-full rounded-tl-full text-font-15 h-10 py-2 pl-5 w-full" placeholder="Search for Products">
-                        <button class="btn btn-primary bg-primary px-3 py-2 h-10 rounded-br-full rounded-tr-full"><span class="ec ec-search text-font-24 text-green-022" aria-hidden="true"></span></button>
-                    </div>
-                </form>
+        <div class="container mx-auto">
+            <div class="flex items-center py-2 xl:py-8">
+                <!-- Logo -->
+                <nav class="max-width-270">           
+                    <a class="logo" href="/">
+                        <img src="/images/logo-01.png" alt="">
+                    </a>
+                </nav>
+
+                <!-- search -->
+                <div class="w-3/6">
+                    <form action="">
+                        <div class="input-group flex">
+                            <input type="search" class="w-2/3 border-2 border-primary rounded-bl-full rounded-tl-full text-font-15 h-10 py-2 pl-5 w-full" placeholder="Search for Products">
+                            <button class="btn btn-primary bg-primary px-3 py-2 h-10 rounded-br-full rounded-tr-full"><span class="ec ec-search text-font-24 text-green-022" aria-hidden="true"></span></button>
+                        </div>
+                    </form>
+                </div>
+
+                <!-- cart -->
+                <div class="w-1/6 justify-end">
+                    <a class="relative" href="/">
+                        <span class="text-font-24 text-green-334 ec ec-shopping-bag"></span>
+                        <span class="text-green-334 bg-primary w-5 h-5 absolute flex justify-center items-center rounded-full text-font-13 font-bold top-2.5 left-3.5">0</span>
+                    </a>
+                </div>   
             </div>
-  
-            <!-- Menu chính -->
-            <!-- <ul class="hidden md:flex space-x-8">
-                <li><a href="#" class="hover:text-blue-500">Trang Chủ</a></li>
-                <li><a href="#" class="hover:text-blue-500">Giới Thiệu</a></li>
-                <li><a href="#" class="hover:text-blue-500">Dịch Vụ</a></li>
-                <li><a href="#" class="hover:text-blue-500">Liên Hệ</a></li>
-            </ul> -->
+            <!-- menu -->
+            <div class="flex items-center">
+                <div class="max-width-270">
+                    <button type="button" class="flex max-width-270 btn btn-primary bg-primary py-4 px-6 rounded-t-lg items-center leading-none font-bold">
+                        <span class="fa fa-list-ul mr-2"></span>
+                        <span class="pl-1">All Departments</span>
+                    </button>
+                </div>
+                <div class="pl-7">
+                    <ul class="hidden md:flex space-x-8">
+                        <li><a href="#" class="font-black hover:text-black">Home</a></li>
+                        <li><a href="#" class="font-black hover:text-black">About Us</a></li>
+                        <li><a href="#" class="font-black hover:text-black">Contact</a></li>
+                        <li><a href="#" class="font-black hover:text-black">Toll free: 1-855-855-5888</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+            
   
             <!-- Nút Menu cho thiết bị di động -->
             <!-- <button @click="toggleMenu" class="md:hidden text-gray-600 focus:outline-none">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
                 </svg>
-            </button> -->        
-        </div>
-        
+            </button> -->       
   
       <!-- Menu di động -->
       <!-- <div v-if="isOpen" class="md:hidden bg-gray-100 px-6 py-4">
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,27 +8,26 @@ module.exports = {
     ],
     theme: {
         extend: {
-            container: {
-                center: true,             
+            fontSize: {
+                'font-13' : '0.81288rem',
+                'font-15' : '0.938rem',
+                'font-18' : '1.12525rem !important',
+                'font-24' : '1.49975rem !important',
+            },
+            colors: {
+                'primary' : '#20d600',
+                'white' : '#ffffff',
+                'purple' : '#3f3cbb',
+                'gray-110' : '#7c7c7c',
+                'gray-100' : '#e8e8e8',
+                'gray-90' : '#333e48',
+                'gray-50' : '#959595',
+                'gray-5' : '#cad5d5',
+                'green-334' : '#334141',
+                'green-022' : '#1e2022',
             }
-        },
-        fontSize: {
-            'font-13' : '0.81288rem',
-            'font-15' : '0.938rem',
-            'font-18' : '1.12525rem !important',
-            'font-24' : '1.49975rem !important',
-        },
-        colors: {
-            'primary' : '#20d600',
-            'white' : '#ffffff',
-            'purple' : '#3f3cbb',
-            'gray-110' : '#7c7c7c',
-            'gray-100' : '#e8e8e8',
-            'gray-50' : '#959595',
-            'gray-5' : '#cad5d5',
-            'green-334' : '#334141',
-            'green-022' : '#1e2022',
         }
+        
     },
     plugins: [],
   }

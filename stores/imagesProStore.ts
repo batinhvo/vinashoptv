a36ba7fb8fc238bbb4e7a8b2cb279be4d96507d1
VoBaTinh <batinhvo@gmail.com>
2025-02-11 20:54:49 +0700

    hydra mismatch

--- stores/imagesProStore.ts
+++ stores/imagesProStore.ts
@@ -34,3 +34,15 @@ export const useImagesProduct = defineStore('imagesProduct', () => {
 
     return { dataImgCache, fetchImagesProduct, error };
 });
+
+
+/*
+
+Mặc dù useAsyncData đã có cơ chế cache dữ liệu, nhưng việc tạo thêm dataImgCache giúp tối ưu việc sử dụng bộ nhớ và giảm số lần gọi API. 
+dataImgCache cung cấp cơ chế lưu trữ riêng cho hình ảnh, giúp tái sử dụng hình ảnh đã tải mà không cần gọi lại API, ngay cả khi người dùng 
+truy cập vào trang mới. Điều này đặc biệt hữu ích khi muốn duy trì dữ liệu trong bộ nhớ lâu dài, giúp tiết kiệm tài nguyên và cải thiện hiệu suất.
+
+useAsyncData thường được dùng để tải dữ liệu khi component được khởi tạo hoặc khi có sự thay đổi của tham số, và dữ liệu đó có thể được xem là tạm thời.
+Trong khi đó, dataImgCache được dùng để lưu trữ kết quả đã tải, đảm bảo rằng các dữ liệu hình ảnh đã được lấy từ API sẽ tồn tại cho đến khi bạn quyết định xóa hay cập nhật cache đó.
+
+*/
--- stores/stateStore.ts
+++ stores/stateStore.ts
@@ -58,3 +58,21 @@ export const useStateStore = defineStore('states', () => {
 // Lỗi: Component is already mounted, please use $fetch instead.
 // Lỗi này xuất hiện vì trong Nuxt 3, useFetch được khuyến khích dùng trong lifecycle hooks trước khi component được mount 
 // (ví dụ như setup hoặc onBeforeMount). Khi component đã được mount rồi, bạn cần dùng $fetch thay cho useFetch.
+
+
+/*
+
+Nguyên nhân gây ra lỗi Hydration Mismatch
+Dữ liệu không nhất quán giữa SSR và client:
+
+Nếu dữ liệu sử dụng để render giao diện thay đổi giữa quá trình render trên server và quá trình render trên client (ví dụ: sử dụng dữ liệu lấy từ API mà không đồng bộ, hoặc tính toán có sự thay đổi như new Date(), Math.random(),…).
+Sử dụng các API chỉ có trên client:
+
+Các đối tượng như window, document hay các tính năng chỉ có trên trình duyệt nếu được sử dụng trong quá trình render phía server sẽ gây ra sự khác biệt.
+Thành phần (component) không đồng nhất:
+
+Các component có logic render phụ thuộc vào môi trường (server/client) hoặc có sự thay đổi trạng thái không được đồng bộ giữa server và client.
+Thay đổi DOM không được kiểm soát:
+
+Nếu có các thay đổi DOM xảy ra ngay sau khi render SSR (ví dụ: thông qua các hiệu ứng hay plugin bên ngoài) thì có thể gây ra sự không khớp khi Vue cố gắng “hydrate” DOM đó.
+*/

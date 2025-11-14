2e48308f8b980d2c83e0feba2c2570151b0d1d31
batinhvo <batinhvo@gmail.com>
2025-11-14 17:20:08 +0700

    menu

--- components/menu/ContentMenuBody.vue
+++ components/menu/ContentMenuBody.vue
@@ -1,7 +1,8 @@
 <template>
     <li>
         <div class="border-b border-gray-100 px-2" v-if="cate.name !== 'SPECIAL'">
-            <div class="cursor-pointer flex justify-between cates-center hover:bg-neutral-100 px-4 py-2 group" @click.prevent="toggleMenu(cate.name)">
+
+            <div v-if="cate.children && cate.children.length > 0" class="cursor-pointer flex justify-between cates-center hover:bg-neutral-100 px-4 py-2 group" @click.prevent="toggleMenu(cate.name)">
                 <NuxtLink v-if="cate.status === 'active' && cate.slug" prefetch
                 :to="`/categories/${cate.slug}`" 
                 class="group-hover:font-bold hover:text-primary" 
@@ -12,6 +13,14 @@
                     <i class="text-xs fa" :class="[openCategory === cate.name ? 'fa-angle-up' : 'fa-angle-down']"></i>
                 </button>
             </div>  
+
+            <NuxtLink v-if="cate.children && cate.children.length == 0" :to="`/categories/${cate.slug}`" class="cursor-pointer flex justify-between cates-center hover:bg-neutral-100 px-4 py-2 group">
+                <div v-if="cate.status === 'active' && cate.slug" prefetch               
+                    class="group-hover:font-bold group-hover:text-primary" 
+                    :class="[openCategory === cate.name ? 'font-bold' : '']"> 
+                        {{ cate.name }}
+                </div>             
+            </NuxtLink>  
 
             <!-- Submenu -->
             <div v-if="cate.children && cate.children.length > 0" v-show="openCategory === cate.name">
